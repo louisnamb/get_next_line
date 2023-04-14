0ac8bdc88a9e867a7b85d974e18dabe6451c1a69
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-04-14 15:25:01 +0930

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,46 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:49:10 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/04/13 16:18:41 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/04/14 15:23:20 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 
-
-int main()
+int get_next_line(int fd)
 {
-	char buffer[1024] = "When you invoke GCC, it normally does preprocessing, compilation, assembly and linking. The overall options allow you to stop this process at an intermediate stage. For example, the -c option says not to run the linker.Then the output consists of object files output by the assembler.";
-	int fd = open("/Users/lnambaji/Documents/Cursus/get_next_line/example.txt", O_RDWR);
-	write(1, &buffer, strlen(buffer));
-	printf("fd: %d", fd);
-	close(fd);
+    char	buffer[1024];
+    ssize_t	bytes_read;
+	int		i;
+	char	*new_buff;
+
+    if (fd == -1) {
+		perror("Couldn't open the file. Try again.");
+		return (1);
+    }
+	i = 0;
+    bytes_read = read(fd, buffer, sizeof(buffer));
+	new_buff = malloc(sizeof(char *) * sizeof(buffer) + 1);
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+	{
+		new_buff[i] = buffer[i];
+		i++;
+	}
+	printf("bytes allocated: %zu str:\n %s\n", sizeof(buffer), new_buff);
+	printf("\n\n");
+	close(fd); // close file when done
+    return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+	fd = open("/Users/lnambaji/Documents/Cursus/get_next_line/example.txt", O_RDONLY);
+	get_next_line(fd);
 	return (0);
 }
 
@@ -35,4 +57,14 @@ Get_next_line notes:
 - next time you run get_next_line, its going to start after the newline character
 	hence the static variable
 - for it to print the rest (until the newline character)
+
+Research areas:
+- research fgets() function
+
+Steps:
+- Open the file and create a pointer to the first character of the file
+- store buffer sized characters in the buffer variable, including the 
+  newline character. 
+- print up to newline characters. free whats left of the newline character
+  and keep whats right of it
 */
