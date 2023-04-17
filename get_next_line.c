0eae3411f8b97d02a0ba917bd8ab568cbfe8a833
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-04-17 11:06:42 +0930

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:49:10 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/04/14 15:23:20 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/04/17 11:05:40 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,43 @@
 
 int get_next_line(int fd)
 {
-    char	buffer[1024];
-    ssize_t	bytes_read;
-	int		i;
-	char	*new_buff;
+    char			buffer[1024];
+    size_t			bytes_read;
+	static int		i;
+	char			*new_buff;
+	static int		j;
 
     if (fd == -1) {
 		perror("Couldn't open the file. Try again.");
 		return (1);
     }
 	i = 0;
-    bytes_read = read(fd, buffer, sizeof(buffer));
-	new_buff = malloc(sizeof(char *) * sizeof(buffer) + 1);
-	while (buffer[i] != '\n' && buffer[i] != '\0')
+	bytes_read = read(fd, buffer, 39);
+	while (buffer[i] && i <= sizeof(buffer))
+	{
+		++i;
+		//printf("\t\ti: %d - %c\n", i, buffer[i]);
+		if (buffer[i] == '\n')
+			break;
+	}
+	new_buff = malloc(sizeof(char) * (i + 1));
+    bytes_read = read(fd, new_buff, sizeof(new_buff));
+	//new_buff[sizeof(buffer)] = '\0';
+	printf("buffer: %s new_buff: %s\n", buffer, new_buff);
+	/*while (buffer[i] != '\0' && i < sizeof(buffer))
 	{
 		new_buff[i] = buffer[i];
 		i++;
-	}
-	printf("bytes allocated: %zu str:\n %s\n", sizeof(buffer), new_buff);
+	}*/
+	j = 0;
+	//printf("bytes allocated: %zu str:\n %s\n", sizeof(buffer), new_buff);
 	printf("\n\n");
+	free(new_buff);
 	close(fd); // close file when done
     return 0;
 }
 
-
-int main(int argc, char **argv)
+int main()
 {
 	int fd;
 	fd = open("/Users/lnambaji/Documents/Cursus/get_next_line/example.txt", O_RDONLY);
